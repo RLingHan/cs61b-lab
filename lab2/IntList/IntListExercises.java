4678b95b09f43997f87d319c9c3ec78193803829
RLingHan <3163857074@qq.com>
2026-03-13 20:54:01 +0800

    finish lab2

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -10,9 +10,13 @@ public class IntListExercises {
      */
     public static void addConstant(IntList lst, int c) {
         IntList head = lst;
-        while (head.rest != null) {
-            head.first += c;
-            head = head.rest;
+//        while (head.rest != null) {
+//            head.first += c;
+//            head = head.rest;
+//        }
+        head.first +=c ;
+        if (head.rest != null) {
+            addConstant(head.rest, c);
         }
     }
 
@@ -51,7 +55,7 @@ public class IntListExercises {
      */
     public static boolean firstDigitEqualsLastDigit(int x) {
         int lastDigit = x % 10;
-        while (x > 10) {
+        while (x >= 10) {
             x = x / 10;
         }
         int firstDigit = x % 10;
@@ -77,6 +81,6 @@ public class IntListExercises {
             lst.first *= lst.first;
         }
 
-        return currElemIsPrime || squarePrimes(lst.rest);
+        return squarePrimes(lst.rest) || currElemIsPrime;
     }
 }
--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,20 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimesSimple2() {
+        IntList lst = IntList.of(0, 1, 2, 18, 19);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("0 -> 1 -> 4 -> 18 -> 361", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimesSimple3() {
+        IntList lst = IntList.of(0, 1, 4, 18, 19);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("0 -> 1 -> 4 -> 18 -> 361", lst.toString());
+        assertTrue(changed);
+    }
 }
